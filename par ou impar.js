eec76bf1117afffb74900433b3b57faace3536f1
LuizDev <102246451+LuizDCarvalho@users.noreply.github.com>
2026-04-24 20:40:18 -0300

    Add files via upload

---
+++ par ou impar.js
@@ -0,0 +1,11 @@
+function checkNum(num){
+    if(num % 2 === 0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+console.log(checkNum(1)); // Output: "Odd"
+console.log(checkNum(2)); // Output: "Even"
+
